27af1a15de109c4a27c51b6e7fce759a61954279
blueleader07 <sean.mckeon07@gmail.com>
2013-10-24 14:57:49 -0400

    Update formatter.java

--- formatter.java
+++ formatter.java
@@ -1,12 +1,13 @@
-for(int j=0; j<34; j++){
-	s+=(t[j]*4)/5
+public class Part {
+	private String m_dsc;
+	void setName(String name){
+		m_dsc = name;
+	}
 }
 
-int realDaysPerIdealDay = 4;
-const int WORK_DAYS_PER_WEEK = 5;
-int sum = 0;
-for(int j=0; j<NUMBER_OF_TASKS; j++){
-	int realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
-	int realTaskWeeks = (realdays / WORK_DAYS_PER_WEEK);
-	sum += realTaskWeeks;
+public class Part {
+	private String description;
+	void setDescription(String description){
+		this.description = description;
+	}
 }
