28fa1c0f425d5da364ef535323c1a1ba532675b5
blueleader07 <sean.mckeon07@gmail.com>
2013-10-24 14:45:08 -0400

    Update formatter.java

--- formatter.java
+++ formatter.java
@@ -1,13 +1,12 @@
-class DtaRcrd102 {
-	private Date genymdhms;
-	private Date modymdhms;
-	private final String recordId = "102";
-	/* ... */
+for(int j=0; j<34; j++){
+	s+=(t[j]*4)/5
 }
 
-class Customer {
-	private Date generationTimestamp;
-	private Date modificationTimestamp;
-	private final String recordId = "102";
-	/* ... */
+int realDaysPerIdealDay = 4;
+const int WORK_DAYS_PER_WEEK = 5;
+int sum = 0;
+for(int j=0; j<NUMBER_OF_TASKS; j++){
+	int realTaskDays = taskEstimate[j] * realDaysPerIdealDay;
+	int realTaskWeeks = (realdays / WORK_DAYS_PER_WEEK);
+	sum += realTaskWeeks;
 }
