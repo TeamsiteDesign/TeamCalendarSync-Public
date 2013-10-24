bbc38a9d7ff66a3e4e2917baf64acc280c6d30d7
blueleader07 <sean.mckeon07@gmail.com>
2013-10-24 15:20:39 -0400

    Update formatter.java

--- formatter.java
+++ formatter.java
@@ -1,13 +1,2 @@
-public class Part {
-	private String m_dsc;
-	void setName(String name){
-		m_dsc = name;
-	}
-}
-
-public class Part {
-	private String description;
-	void setDescription(String description){
-		this.description = description;
-	}
-}
+Complex fulcrumPoint = Complex.FromRealNumber(23.0);
+Complex fulcrumPoint = new Complex(23.0);
